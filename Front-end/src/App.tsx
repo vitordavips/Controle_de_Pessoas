4d74d7430e9a7f66e118b243ffcd10f9727acdee
vitordavips <vitordavidps@gmail.com>
2025-07-30 22:32:32 -0400

    começando a criar a tela de login

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -2,10 +2,13 @@ import Form from "./components/Form"
 
 function App() {
   return (
-    <>
-      <h1>Login</h1>
-      <Form />
-    </>
+    <div className="border-y-indigo-200 min-h-screen min-w-full flex flex-col-reverse items-center justify-center">
+      <div className=" w-96 mt-4 bg-stone-200 px-4 py-5 rounded-lg">
+        <h1 className="font-bold text-[2rem] text-gray-600">Login</h1>
+          <Form />
+           <p><a href="#">Cadastra-se</a></p>
+      </div>
+    </div>
   )
 }
 
--- Front-end/src/components/Form.tsx
+++ Front-end/src/components/Form.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <form>
+      <div>
+        <label htmlFor="">E-mail</label>
+        <input type="email" name="email" id="" placeholder='Digite o seu e-mail'/>
+      </div>
+      <div>
+        <label htmlFor="">Senha</label>
+        <input type="password" name="senha" id="" placeholder='Digite a sua senha' />
+      </div>
+      <button>Login</button>
+    </form>
   )
 }
 
